7a337b395c6360aa8dd77cf11a687e334a6787c0
Robert Castley <robert.castley@gmail.com>
2025-03-25 16:18:38 +0000

    Comment out terraform

--- player-cloud/entrypoint.destroy.sh
+++ player-cloud/entrypoint.destroy.sh
@@ -4,5 +4,5 @@ set -euxo pipefail
 
 echo "secret_suffix = \"${TF_VAR_player_name}\"" > backend.hcl
 
-terraform init -input=false -backend-config=backend.hcl
-terraform destroy -auto-approve
+#terraform init -input=false -backend-config=backend.hcl
+#terraform destroy -auto-approve
