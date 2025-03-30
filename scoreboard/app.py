44875fe9f96604afcf6cab3c8d6a2fc0944aa56b
Robert Castley <robert.castley@gmail.com>
2025-03-30 16:11:35 +0100

    Clean up build messages

--- scoreboard/app.py
+++ scoreboard/app.py
@@ -4,4 +4,4 @@ from src import create_app
 
 if __name__ == "__main__":
     app = create_app()
-    serve(app, port=5000, threads=32, connection_limit=1000, backlog=2048)
+    serve(app, port=5000, threads=32)
